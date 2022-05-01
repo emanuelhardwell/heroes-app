365db83b004d0112329e4ccc6211528e28e5405e
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-01 11:23:17 -0500

    Se creo el Navbar de la App y se configuro el Router

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,9 +1,70 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Heroes App
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                exact
+                activeClassName="active"
+                className="nav-link"
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                activeClassName="active"
+                className="nav-link"
+                to="/dc"
+              >
+                DC
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                activeClassName="active"
+                className="nav-link"
+                to="/marvel"
+              >
+                Marvel
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                activeClassName="active"
+                className="nav-link  "
+                to="/login"
+              >
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { NavBar } from "../components/ui/NavBar";
 
 export const AppRouter = () => {
   return (
-    <div>
-      <h1> AppRouter </h1>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={MarvelScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
