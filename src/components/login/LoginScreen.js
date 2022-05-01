562600c0df69aeb6f0d69c2bb7cc86f7799c2c9a
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-01 13:22:53 -0500

    Se creo un Login simulado para entender el HISTORY.PUSH(/)

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,33 @@
 import React from "react";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    // console.log(history);
+    // usando  history.push("/") puedo regresar a la pagina anterior
+    // history.push("/");
+
+    //  usando  history.replace("/") NO puedo regresar a la pagina anterior
+    //  se utiilza normalmente en el LOGIN
+    history.replace("/");
+  };
   return (
-    <div>
-      <h1> LoginScreen </h1>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <div className="card">
+            <div className="card-body">
+              <h1> LoginScreen </h1>
+              <hr />
+              <div className="mb-2">
+                <button className="btn btn-primary" onClick={handleLogin}>
+                  {" "}
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
