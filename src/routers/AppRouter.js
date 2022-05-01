08904d231bfac4e082b85a12f7923448a327320f
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-01 13:04:46 -0500

    Se crearon 2 Router y para el login y uno para el Dashboard

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { NavBar } from "../components/ui/NavBar";
+
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <NavBar />
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route exact path="/" component={MarvelScreen} />
           <Route exact path="/login" component={LoginScreen} />
+          <Route path="/" component={DashboardRoutes} />
         </Switch>
       </div>
     </Router>
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { DcScreen } from "../components/dc/DcScreen";
+import { HeroScreen } from "../components/heroes/HeroScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { NavBar } from "../components/ui/NavBar";
+
+export const DashboardRoutes = () => {
+  return (
+    <>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route exact path="/marvel" component={MarvelScreen} />
+          <Route exact path="/heroe/:heroeId " component={HeroScreen} />
+          <Route exact path="/dc" component={DcScreen} />
+          <Redirect to="/marvel" />
+        </Switch>
+      </div>
+    </>
+  );
+};
