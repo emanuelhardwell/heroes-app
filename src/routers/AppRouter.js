171a6459089f71c185d5214ea6ac92fe5e08b652
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-02 15:07:54 -0500

    Se creo la funcionalidad de Rutas Protegidas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRouter } from "./PrivateRouter";
 
 export const AppRouter = () => {
+  const {
+    user: { logged },
+  } = useContext(AuthContext);
   return (
     <Router>
       <div>
@@ -12,7 +17,13 @@ export const AppRouter = () => {
             renders the first one that matches the current URL. */}
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashboardRoutes} />
+          {/* <Route path="/" component={DashboardRoutes} /> */}
+
+          <PrivateRouter
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={logged}
+          />
         </Switch>
       </div>
     </Router>
